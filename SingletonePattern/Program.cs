51a9c0dfd3784c538184c4ad0c688d62e6867190
Dzmitry.Shchamialiou <Dzmitry.Shchamialiou@itechart-group.com>
2019-08-27 12:25:27 +0300

    Add project files.

---
+++ SingletonePattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SingletonePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
